baea3bfaa9a9dc202f5760297b5eb5ff0cc5c1b6
HungK <80261955+kimhung1100@users.noreply.github.com>
2023-10-18 15:57:37 +0700

    writting uttt muzero

--- utttpy/game/ultimate_tic_tac_toe.py
+++ utttpy/game/ultimate_tic_tac_toe.py
@@ -16,7 +16,6 @@ from utttpy.game.constants import (
 
 
 class UltimateTicTacToe:
-
     def __init__(self, state: Optional[bytearray] = None):
         if state:
             self.state = state
@@ -109,28 +108,26 @@ class UltimateTicTacToe:
         offset = subgame * 9
         return (
             (
-                symbol == state[offset + 4] and
-                (
-                    symbol == state[offset + 0] == state[offset + 8] or
-                    symbol == state[offset + 2] == state[offset + 6] or
-                    symbol == state[offset + 1] == state[offset + 7] or
-                    symbol == state[offset + 3] == state[offset + 5]
+                symbol == state[offset + 4]
+                and (
+                    symbol == state[offset + 0] == state[offset + 8]
+                    or symbol == state[offset + 2] == state[offset + 6]
+                    or symbol == state[offset + 1] == state[offset + 7]
+                    or symbol == state[offset + 3] == state[offset + 5]
                 )
             )
-            or
-            (
-                symbol == state[offset + 0] and
-                (
-                    symbol == state[offset + 1] == state[offset + 2] or
-                    symbol == state[offset + 3] == state[offset + 6]
+            or (
+                symbol == state[offset + 0]
+                and (
+                    symbol == state[offset + 1] == state[offset + 2]
+                    or symbol == state[offset + 3] == state[offset + 6]
                 )
             )
-            or
-            (
-                symbol == state[offset + 8] and
-                (
-                    symbol == state[offset + 2] == state[offset + 5] or
-                    symbol == state[offset + 6] == state[offset + 7]
+            or (
+                symbol == state[offset + 8]
+                and (
+                    symbol == state[offset + 2] == state[offset + 5]
+                    or symbol == state[offset + 6] == state[offset + 7]
                 )
             )
         )
@@ -191,19 +188,33 @@ class UltimateTicTacToe:
             o_w = self._is_winning_position(symbol=O_STATE_VALUE, subgame=subgame)
             full = self._is_full(subgame=subgame)
             if x_w and o_w:
-                raise UltimateTicTacToeError(f"X and O have winning positions on subgame={subgame}")
+                raise UltimateTicTacToeError(
+                    f"X and O have winning positions on subgame={subgame}"
+                )
             if x_w and self.state[81 + subgame] != X_STATE_VALUE:
-                raise UltimateTicTacToeError(f"X won subgame={subgame}, but supergame is not updated")
+                raise UltimateTicTacToeError(
+                    f"X won subgame={subgame}, but supergame is not updated"
+                )
             if o_w and self.state[81 + subgame] != O_STATE_VALUE:
-                raise UltimateTicTacToeError(f"O won subgame={subgame}, but supergame is not updated")
-            if full and self.state[81 + subgame] != DRAW_STATE_VALUE and not (x_w or o_w):
-                raise UltimateTicTacToeError(f"DRAW on subgame={subgame}, but supergame is not updated")
+                raise UltimateTicTacToeError(
+                    f"O won subgame={subgame}, but supergame is not updated"
+                )
+            if (
+                full
+                and self.state[81 + subgame] != DRAW_STATE_VALUE
+                and not (x_w or o_w)
+            ):
+                raise UltimateTicTacToeError(
+                    f"DRAW on subgame={subgame}, but supergame is not updated"
+                )
 
     def _verify_constraint(self) -> None:
         if not (self.is_constrained() or self.is_unconstrained()):
             raise UltimateTicTacToeError(f"invalid constraint={self.constraint}")
         if self.is_constrained() and self.state[81 + self.constraint]:
-            raise UltimateTicTacToeError(f"constraint={self.constraint} points to terminated subgame")
+            raise UltimateTicTacToeError(
+                f"constraint={self.constraint} points to terminated subgame"
+            )
 
     def _verify_action(self, action: Action) -> None:
         illegal_action = f"Illegal {action} - "
@@ -212,69 +223,75 @@ class UltimateTicTacToe:
         if self.is_next_symbol_O() and not action.is_symbol_O():
             raise UltimateTicTacToeError(illegal_action + "next move belongs to O")
         if not (0 <= action.index < 81):
-            raise UltimateTicTacToeError(illegal_action + "index outside the valid range")
+            raise UltimateTicTacToeError(
+                illegal_action + "index outside the valid range"
+            )
         if self.is_constrained() and self.constraint != action.index // 9:
-            raise UltimateTicTacToeError(illegal_action + f"violated constraint={self.constraint}")
+            raise UltimateTicTacToeError(
+                illegal_action + f"violated constraint={self.constraint}"
+            )
         if self.state[81 + action.index // 9]:
-            raise UltimateTicTacToeError(illegal_action + "index from terminated subgame")
+            raise UltimateTicTacToeError(
+                illegal_action + "index from terminated subgame"
+            )
         if self.state[action.index]:
             raise UltimateTicTacToeError(illegal_action + "index is already taken")
 
     def __str__(self):
         state_values_map = {
-            X_STATE_VALUE: 'X',
-            O_STATE_VALUE: 'O',
-            DRAW_STATE_VALUE: '=',
-            0: '-',
+            X_STATE_VALUE: "X",
+            O_STATE_VALUE: "O",
+            DRAW_STATE_VALUE: "=",
+            0: "-",
         }
         subgames = [state_values_map[s] for s in self.state[0:81]]
         supergame = [state_values_map[s] for s in self.state[81:90]]
         if not self.is_terminated():
             for legal_index in self.get_legal_indexes():
-                subgames[legal_index] = '•'
+                subgames[legal_index] = "•"
             if self.is_constrained():
-                supergame[self.constraint] = '•'
+                supergame[self.constraint] = "•"
             elif self.is_unconstrained():
-                supergame = ['•' if s == '-' else s for s in supergame]
-        sb = lambda l, r: ' '.join(subgames[l : r + 1])
-        sp = lambda l, r: ' '.join(supergame[l : r + 1])
+                supergame = ["•" if s == "-" else s for s in supergame]
+        sb = lambda l, r: " ".join(subgames[l : r + 1])
+        sp = lambda l, r: " ".join(supergame[l : r + 1])
         subgames = [
-            '    0 1 2   3 4 5   6 7 8',
-            '  0 ' + sb(0, 2) + ' │ ' + sb(9, 11) + ' │ ' + sb(18, 20),
-            '  1 ' + sb(3, 5) + ' │ ' + sb(12, 14) + ' │ ' + sb(21, 23),
-            '  2 ' + sb(6, 8) + ' │ ' + sb(15, 17) + ' │ ' + sb(24, 26),
-            '    ' + '—' * 21,
-            '  3 ' + sb(27, 29) + ' │ ' + sb(36, 38) + ' │ ' + sb(45, 47),
-            '  4 ' + sb(30, 32) + ' │ ' + sb(39, 41) + ' │ ' + sb(48, 50),
-            '  5 ' + sb(33, 35) + ' │ ' + sb(42, 44) + ' │ ' + sb(51, 53),
-            '    ' + '—' * 21,
-            '  6 ' + sb(54, 56) + ' │ ' + sb(63, 65) + ' │ ' + sb(72, 74),
-            '  7 ' + sb(57, 59) + ' │ ' + sb(66, 68) + ' │ ' + sb(75, 77),
-            '  8 ' + sb(60, 62) + ' │ ' + sb(69, 71) + ' │ ' + sb(78, 80),
+            "    0 1 2   3 4 5   6 7 8",
+            "  0 " + sb(0, 2) + " │ " + sb(9, 11) + " │ " + sb(18, 20),
+            "  1 " + sb(3, 5) + " │ " + sb(12, 14) + " │ " + sb(21, 23),
+            "  2 " + sb(6, 8) + " │ " + sb(15, 17) + " │ " + sb(24, 26),
+            "    " + "—" * 21,
+            "  3 " + sb(27, 29) + " │ " + sb(36, 38) + " │ " + sb(45, 47),
+            "  4 " + sb(30, 32) + " │ " + sb(39, 41) + " │ " + sb(48, 50),
+            "  5 " + sb(33, 35) + " │ " + sb(42, 44) + " │ " + sb(51, 53),
+            "    " + "—" * 21,
+            "  6 " + sb(54, 56) + " │ " + sb(63, 65) + " │ " + sb(72, 74),
+            "  7 " + sb(57, 59) + " │ " + sb(66, 68) + " │ " + sb(75, 77),
+            "  8 " + sb(60, 62) + " │ " + sb(69, 71) + " │ " + sb(78, 80),
         ]
         supergame = [
-            '  ' + sp(0, 2),
-            '  ' + sp(3, 5),
-            '  ' + sp(6, 8),
+            "  " + sp(0, 2),
+            "  " + sp(3, 5),
+            "  " + sp(6, 8),
         ]
-        subgames = '\n'.join(subgames)
-        supergame = '\n'.join(supergame)
+        subgames = "\n".join(subgames)
+        supergame = "\n".join(supergame)
         next_symbol = state_values_map[self.next_symbol]
-        constraint = 'None' if self.is_unconstrained() else str(self.constraint)
-        result = 'None'
+        constraint = "None" if self.is_unconstrained() else str(self.constraint)
+        result = "None"
         if self.is_result_X():
-            result = 'X_WON'
+            result = "X_WON"
         elif self.is_result_O():
-            result = 'O_WON'
+            result = "O_WON"
         elif self.is_result_draw():
-            result = 'DRAW'
-        output = '{cls}(\n'
-        output += '  subgames:\n{subgames}\n'
+            result = "DRAW"
+        output = "{cls}(\n"
+        output += "  subgames:\n{subgames}\n"
         if not self.is_terminated():
-            output += '  next_symbol: {next_symbol}\n'
-            output += '  constraint: {constraint}\n'
-        output += '  supergame:\n{supergame}\n'
-        output += '  result: {result}\n)'
+            output += "  next_symbol: {next_symbol}\n"
+            output += "  constraint: {constraint}\n"
+        output += "  supergame:\n{supergame}\n"
+        output += "  result: {result}\n)"
         output = output.format(
             cls=self.__class__.__name__,
             subgames=subgames,
@@ -284,6 +301,13 @@ class UltimateTicTacToe:
             result=result,
         )
         return output
+
+    def get_observation(self):
+        board_player1 = numpy.where(self.board == 1, 1, 0)
+        board_player2 = numpy.where(self.board == -1, 1, 0)
+        board_to_play = numpy.full((3, 3), self.player)
+        return numpy.array([board_player1, board_player2, board_to_play], dtype="int32")
+
 
 
 class UltimateTicTacToeError(Exception):
